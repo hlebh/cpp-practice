4581cc7c87dd48e2afcabce2efc921eca0d8116c
Gleb Gribovskii <286166424+hlebh@users.noreply.github.com>
2026-06-25 01:35:37 +0000

    Practice of 3 chapters

--- src/Ch01/01_02b/CodeDemo.cpp
+++ src/Ch01/01_02b/CodeDemo.cpp
@@ -1,3 +1,21 @@
 // Complete Guide to C++ Programming Foundations
 // Exercise 01_02
 // Hello World, by Eduardo Corpeño 
+
+#include <iostream>
+#include <cstdio>
+
+int add(int a, int b)
+{
+    return a+b;
+}
+
+int main()
+{
+    int age;
+    std::cout << "What is you age?" << std::endl;
+    std::cin >> age;
+    std::cout << "You are " << age << " years old";
+    return 0;
+}
+
